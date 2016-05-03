1d4d65cdb6a9e93c1e232288ce6fc3cbb8c556fd
Gabriel Csapo <gabecsapo@gmail.com>
2016-05-02 22:44:40 -0700

    completes TODO: refactor this out into routes/index

--- index.js
+++ index.js
@@ -6,67 +6,21 @@ var path = require('path');
 var mkdirp = require('mkdirp');
 var conf = require('./config/config')();
 var log = require('./lib/log')(conf);
-var kue = require('kue');
 var levelup = require('levelup');
-var responseTime = require('response-time');
+
+mkdirp(conf.get('storage'), function (err) {
+    if (err) { log.error(err.toString()); }
+});
 
 var app = express();
 app.db = levelup('./db');
 app.conf = conf;
 app.log = log;
 
-var Project = require('./controllers/project')(app, log, conf);
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use('/static', express.static(path.join(__dirname, 'views/static')));
 
-// TODO: refactor this out into routes/index
-app.use(responseTime(function(req, res, time) {
-    var file;
-    if(req.route) {
-        if(req.route.path == '/ui/download/:folder/:file') {
-            file = req.originalUrl.replace('/ui/download/', '');
-            app.db.get(file+'-download-time', function(err, value) {
-                if (err) {
-                    app.db.put(file+'-download-time', time, function(err) {
-                        if (err) { log.error(err.toString()); }
-                        return;
-                    });
-                } else {
-                    value = parseInt(value);
-                    value += time;
-                    app.db.put(file+'-download-time', value, function(err) {
-                        if (err) { log.error(err.toString()); }
-                        return;
-                    });
-                }
-            });
-        } else if(req.route.path == '/granary/download') {
-            var project = Project.getDetails(req.body);
-            file = project.bundlePath;
-            if (req.body.options && req.body.options.production === 'true') {
-                file = project.productionBundlePath;
-            }
-            file = file.substring(file.indexOf(project.name), file.length);
-            app.db.get(file+'-download-time', function(err, value) {
-                if (err) {
-                    app.db.put(file+'-download-time', time, function(err) {
-                        if (err) { log.error(err.toString()); }
-                        return;
-                    });
-                } else {
-                    value = parseInt(value);
-                    value += time;
-                    app.db.put(file+'-download-time', value, function(err) {
-                        if (err) { log.error(err.toString()); }
-                        return;
-                    });
-                }
-            });
-        }    
-    }
-}));
 app.use(bodyParser.json({
     limit: conf.get('limit') + 'kb'
 }));
@@ -75,13 +29,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-mkdirp(conf.get('storage'), function (err) {
-    if (err) { log.error(err.toString()); }
-});
-
 require('./routes/index')(app, log, conf);
-
-app.use('/granaries', kue.app);
 
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,60 @@
+var marked = require('marked');
+var kue = require('kue');
+var responseTime = require('response-time');
+
 module.exports = function(app, log, conf) {
-    var marked = require('marked');
     var Auth = require('../lib/auth')(log, conf);
+    var Project = require('../controllers/project')(app, log, conf);
     var Granary = require('../controllers/granary')(app, log, conf);
     var UI = require('../controllers/ui')(app, log, conf);
+
+    app.use(Auth.middleware);
+
+    app.use(responseTime(function(req, res, time) {
+        var file;
+        if(req.route) {
+            if(req.route.path == '/ui/download/:folder/:file') {
+                file = req.originalUrl.replace('/ui/download/', '');
+                app.db.get(file+'-download-time', function(err, value) {
+                    if (err) {
+                        app.db.put(file+'-download-time', time, function(err) {
+                            if (err) { log.error(err.toString()); }
+                            return;
+                        });
+                    } else {
+                        value = parseInt(value);
+                        value += time;
+                        app.db.put(file+'-download-time', value, function(err) {
+                            if (err) { log.error(err.toString()); }
+                            return;
+                        });
+                    }
+                });
+            } else if(req.route.path == '/granary/download') {
+                var project = Project.getDetails(req.body);
+                file = project.bundlePath;
+                if (req.body.options && req.body.options.production === 'true') {
+                    file = project.productionBundlePath;
+                }
+                file = file.substring(file.indexOf(project.name), file.length);
+                app.db.get(file+'-download-time', function(err, value) {
+                    if (err) {
+                        app.db.put(file+'-download-time', time, function(err) {
+                            if (err) { log.error(err.toString()); }
+                            return;
+                        });
+                    } else {
+                        value = parseInt(value);
+                        value += time;
+                        app.db.put(file+'-download-time', value, function(err) {
+                            if (err) { log.error(err.toString()); }
+                            return;
+                        });
+                    }
+                });
+            }
+        }
+    }));
 
     app.get('/', UI.usage, function(req, res) {
         req.data.marked = marked;
@@ -21,5 +73,5 @@ module.exports = function(app, log, conf) {
     app.post('/granary/download', Granary.download);
     app.post('/granary/track', Granary.track);
 
-    app.use(Auth.middleware);
+    app.use('/granaries', kue.app);
 };
