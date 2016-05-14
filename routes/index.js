39117f52d7311b3fd757699e79b78ed30634697a
Gabriel Csapo <gabecsapo@gmail.com>
2016-05-14 14:24:53 -0700

    bring it on 👊 TODO: refactor this out into routes/api.js TODO: refactor the need to pass log to this, require it from the log file TODO: refactor this into routes/stats.js TODO: this should not be all pages (irrelevant) TODO: refactor this out into routes/ui.js TODO: split the ui routes into another file

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,9 @@
-var marked = require('marked');
-var kue = require('kue');
-var path = require('path');
-var fs = require('fs');
 var responseTime = require('response-time');
 var bodyParser = require('body-parser');
 
 module.exports = function(app, log, conf) {
-    var Auth = require('../lib/auth')();
-    var Project = require('../lib/project')(log, conf);
-    var Stats = require('../lib/stats')(log);
-    var Granary = require('../lib/granary')(log, conf);
-    var UI = require('../lib/ui')();
+    var Project = require('../lib/project')();
+    var Stats = require('../lib/stats')();
 
     app.use(bodyParser.json({
         limit: conf.get('limit') + 'kb'
@@ -22,6 +15,7 @@ module.exports = function(app, log, conf) {
 
     app.use(responseTime(function(req, res, time) {
         var file;
+
         if(req.route) {
             if(req.route.path == '/ui/download/:folder/:file') {
                 file = req.originalUrl.replace('/ui/download/', '');
@@ -48,64 +42,9 @@ module.exports = function(app, log, conf) {
         }
     }));
 
-    // TODO: need to have these routes protected by the basic auth route (right now they have their own password checking...)
-    // TODO: refactor this out into routes/api.js
-    app.post('/granary/check', Granary.check);
-    app.post('/granary/download', Granary.download);
-    app.post('/granary/stats', Granary.stats);
-    app.post('/granary/track', Granary.track);
-
-    // TODO: this should not be all pages
-    app.use(Auth.middleware);
-
-    // TODO: refactor this out into routes/ui.js
-    app.get('/', UI.usage, function(req, res) {
-        req.data.marked = marked;
-        res.render('index', req.data);
-    });
-
-    app.get('/bundles', UI.usage, function(req, res) {
-        res.render('bundles', req.data);
-    });
-
-    app.get('/queue', function(req, res) {
-        res.render('queue');
-    });
-
-    // TODO: refactor this into routes/stats.js
-    app.get('/stats', function(req, res) {
-        res.render('stats');
-    });
-
-    app.get('/stats/downloads_over_time', function(req, res) {
-        Stats.get().then(function(stats) {
-            res.send(stats.downloads_over_time);
-        });
-    })
-
-    // TODO: split the ui routes into another file
-    app.get('/ui/:folder/:file', function(req, res) {
-        var folder = req.params.folder;
-        var file = req.params.file;
-        fs.exists(path.join(conf.get('storage'), folder, file), function(exists) {
-            if(exists) {
-                Project.getStats(path.join(folder, file)).then(function(bundle) {
-                    res.render('bundle', {bundle: bundle});
-                });
-            } else {
-                res.render('error', {
-                  message: 'Bundle does not exist',
-                  error: {
-                      status: 404
-                  }
-                });
-            }
-        });
-    });
-    app.get('/ui/download/:folder/:file', Auth.middleware, UI.download);
-    app.get('/ui/delete/:folder/:file', Auth.logout, Auth.middleware, UI.delete);
-
-    app.use('/kue', kue.app);
+    require('./granary')(app);
+    require('./stats')(app);
+    require('./ui')(app);
 
     app.use(function (req, res, next) {
       var err = new Error('Not Found');
---
+++ routes/ui.js
@@ -0,0 +1,49 @@
+var fs = require('fs');
+var path = require('path');
+var kue = require('kue');
+var marked = require('marked');
+var UI = require('../lib/ui')();
+var Auth = require('../lib/auth')();
+var Project = require('../lib/project')();
+
+var conf = require('../config/config').conf;
+
+module.exports = function(app) {
+    app.use(Auth.middleware);
+
+    app.get('/', UI.usage, function(req, res) {
+        req.data.marked = marked;
+        res.render('index', req.data);
+    });
+
+    app.get('/bundles', UI.usage, function(req, res) {
+        res.render('bundles', req.data);
+    });
+
+    app.get('/queue', function(req, res) {
+        res.render('queue');
+    });
+
+    app.get('/ui/:folder/:file', function(req, res) {
+        var folder = req.params.folder;
+        var file = req.params.file;
+        fs.exists(path.join(conf.get('storage'), folder, file), function(exists) {
+            if(exists) {
+                Project.getStats(path.join(folder, file)).then(function(bundle) {
+                    res.render('bundle', {bundle: bundle});
+                });
+            } else {
+                res.render('error', {
+                  message: 'Bundle does not exist',
+                  error: {
+                      status: 404
+                  }
+                });
+            }
+        });
+    });
+    app.get('/ui/download/:folder/:file', Auth.middleware, UI.download);
+    app.get('/ui/delete/:folder/:file', Auth.logout, Auth.middleware, UI.delete);
+
+    app.use('/kue', kue.app);
+}
