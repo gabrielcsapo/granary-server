2d5013576b1e2ac67e49a7131a3074de74db18cd
Gabriel Csapo <gacsapo@paypalcorp.com>
2016-07-12 13:39:30 -0700

    fixes pathing issue with d3

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.set('view engine', 'pug');
 app.use('/static', express.static(path.resolve(__dirname, 'views/static')));
 app.use('/static/c3', express.static(path.resolve(__dirname, 'node_modules', 'c3')));
 app.use('/static/font-awesome', express.static(path.resolve(__dirname, 'node_modules', 'font-awesome')));
+app.use('/static/d3', express.static(path.resolve(__dirname, 'node_modules', 'd3')));
 
 require('./routes/index')(app, log, conf);
 
